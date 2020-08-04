5b4332a11621ca9e1a5700bc1a8676a5f6098ac3
elias9314 <kuky1993roger@gmail.com>
2020-08-03 19:08:58 -0500

    jjjjj

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -7,10 +7,11 @@ import { GasComponent } from './gas/gas.component';
 import { PedidoComponent } from './pedido/pedido.component';
 import { SucursalComponent } from './sucursal/sucursal.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [ClienteComponent, GasComponent, PedidoComponent, SucursalComponent, SidebarComponent],
+  declarations: [ClienteComponent, GasComponent, PedidoComponent, SucursalComponent, SidebarComponent, NavbarComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule
