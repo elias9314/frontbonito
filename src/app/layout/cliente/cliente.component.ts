cc346203e012cd5d28bbba03f59a9ba95ae63c33
elias9314 <kuky1993roger@gmail.com>
2020-08-22 12:40:57 -0500

    Validaciones

--- src/app/layout/cliente/cliente.component.ts
+++ src/app/layout/cliente/cliente.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Usuario} from '../../models/usuario';
 import { ServiceService} from '../../services/service.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
+import { FormBuilder, FormControl,FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
@@ -12,16 +12,44 @@ export class ClienteComponent implements OnInit {
   usuarios: Array<Usuario>;
   usuarioSeleccionado: Usuario;
   usuario: Usuario;
+  hide : true;
+  private patternnombres: any ="([a-zA-ZÀ-ÿ\u00f1\u00d1\.][^\s]*)+$";
+  private patterncedula : any="[0-9]{7,10}$";
+  private patterncorreo : any="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  private patterntelefono : any="[0-9]{7,10}$";
+  private patterndireccion : any= "([a-zA-ZÀ-ÿ\u00f1\u00d1\.0-9][^\s]*)+$";
 
+  usuarioForm: FormGroup;
   constructor(private service: ServiceService, private modalService: NgbModal) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
     this.getUsuarios();
+    this.formularioTipo();
     this.usuarioSeleccionado = new Usuario();
     this.usuarios = new Array<Usuario>();
     this.usuario = new Usuario();
   }
 
+  formularioTipo() {
+    return this.usuarioForm = new FormGroup({
+      nombre: new FormControl('', [Validators.required, Validators.pattern(this.patternnombres)]),
+      apellido: new FormControl('', [Validators.required, Validators.pattern(this.patternnombres)]),
+      cedula: new FormControl('', [Validators.required, Validators.pattern(this.patterncedula)]),
+      telefono: new FormControl('', [Validators.required, Validators.pattern(this.patterntelefono)]),
+      correo: new FormControl('', [Validators.required, Validators.pattern(this.patterncorreo)]),
+      direccion: new FormControl('', [Validators.required, Validators.pattern(this.patterndireccion)]),    
+      password: new FormControl('', [Validators.required])
+   });
+}
+
+get nombre() {return this.usuarioForm.get('nombre'); }
+get apellido() {return this.usuarioForm.get('apellido'); }
+get cedula() {return this.usuarioForm.get('cedula'); }
+get telefono() {return this.usuarioForm.get('telefono'); }
+get direccion() {return this.usuarioForm.get('direccion'); }
+get correo() {return this.usuarioForm.get('correo'); }
+get password() {return this.usuarioForm.get('password'); }
 
   getUsuarios() {
     this.service.get('/users').subscribe(
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -8,7 +8,7 @@ import { PedidoComponent } from './pedido/pedido.component';
 import { SucursalComponent } from './sucursal/sucursal.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { ClienteSucursalesComponent } from './cliente-sucursales/cliente-sucursales.component';
 import { PedidosClienteComponent } from './pedidos-cliente/pedidos-cliente.component';
 import { ClientesRealizarPedidosComponent } from './clientes-realizar-pedidos/clientes-realizar-pedidos.component';
@@ -27,6 +27,7 @@ import { ConfirmarEntregaComponent } from './confirmar-entrega/confirmar-entrega
     CommonModule,
     LayoutRoutingModule,
     FormsModule,
+    ReactiveFormsModule
     
   ]
 })
